3b9d469c439b21167f16772487b3c14e33196b9c
Theo Sanderson <theo@theo.io>
2021-01-24 22:34:38 +0000

    Remove temporary date

--- src/components/LocalIncidence.jsx
+++ src/components/LocalIncidence.jsx
@@ -3,13 +3,13 @@ import React from "react";
 import LineChart2 from "./LineChart2";
 
 import moment from "moment";
-function LocalIncidence({data,lad,date,name}) {
+function LocalIncidence({ data, lad, date, name }) {
 
-    const lad_data = data.filter((item) => item.location == lad)
-    return(<div>
-        <h2>Local incidences {date}</h2>
-            <p className="lead">Local Authority: {name}</p>
-            {/* {lad.data && (
+  const lad_data = data.filter((item) => item.location == lad)
+  return (<div>
+    <h2>Local incidences</h2>
+    <p className="lead">Local Authority: {name}</p>
+    {/* {lad.data && (
               <LineChart
                 x={lad.data
                   .filter((item) => item.parameter === "lambda")
@@ -25,35 +25,35 @@ function LocalIncidence({data,lad,date,name}) {
                   .map((item) => item.lower)}
               />
             )} */}
-           
-              <LineChart2
-              date = {date}
-                x={lad_data
-                  .filter((item) => item.parameter === "yhat")
-                  .map((item) => moment(item.date).format("YYYY-MM-DD"))}
-                y={lad_data
-                  .filter((item) => item.parameter === "lambda")
-                  .map((item) => item.mean)}
-                upper={lad_data
-                  .filter((item) => item.parameter === "lambda")
-                  .map((item) => item.upper)}
-                lower={lad_data
-                  .filter((item) => item.parameter === "lambda")
-                  .map((item) => item.lower)}
-                y1={lad_data
-                  .filter((item) => item.parameter === "yhat")
-                  .map((item) => item.mean)}
-                upper1={lad_data
-                  .filter((item) => item.parameter === "yhat")
-                  .map((item) => item.upper)}
-                lower1={lad_data
-                  .filter((item) => item.parameter === "yhat")
-                  .map((item) => item.lower)}
-              />
-            
-            </div>
-    );
-  }
+
+    <LineChart2
+      date={date}
+      x={lad_data
+        .filter((item) => item.parameter === "yhat")
+        .map((item) => moment(item.date).format("YYYY-MM-DD"))}
+      y={lad_data
+        .filter((item) => item.parameter === "lambda")
+        .map((item) => item.mean)}
+      upper={lad_data
+        .filter((item) => item.parameter === "lambda")
+        .map((item) => item.upper)}
+      lower={lad_data
+        .filter((item) => item.parameter === "lambda")
+        .map((item) => item.lower)}
+      y1={lad_data
+        .filter((item) => item.parameter === "yhat")
+        .map((item) => item.mean)}
+      upper1={lad_data
+        .filter((item) => item.parameter === "yhat")
+        .map((item) => item.upper)}
+      lower1={lad_data
+        .filter((item) => item.parameter === "yhat")
+        .map((item) => item.lower)}
+    />
+
+  </div>
+  );
+}
 
 
-  export default LocalIncidence;
+export default LocalIncidence;
