a60a165a59fd10a3ec5f50db9ff1188ac23d1882
Theo Sanderson <theo@sndrsn.co.uk>
2021-01-26 17:24:06 +0000

    format (#3)

--- src/components/LocalIncidence.jsx
+++ src/components/LocalIncidence.jsx
@@ -3,52 +3,38 @@ import React from "react";
 import LineChart2 from "./LineChart2";
 
 import moment from "moment";
-function LocalIncidence({ data, lad, date, name }) {
+import memoize from 'memoize-one';
 
-  const lad_data = data.filter((item) => item.location === lad)
+function get_lad_data(dataframe, lad,lineage){
+  console.log('calling get')
+  const lad_data = dataframe.where((item) => item.location === lad ).where((item) => item.parameter === "lambda" ).where((item) => item.lineage === lineage ).toArray()
+  return(lad_data)
+}
+
+var memoized_get_lad_data = memoize(get_lad_data)
+
+function LocalIncidence({ dataframe, lad, date, name,lineage }) {
+  console.log(lad)
+  let lad_data = memoized_get_lad_data(dataframe,lad,lineage)
+  console.log(lad_data)
+  window.df3 = dataframe
+  window.df4 = lad_data
   return (<div>
     <h2>Local incidences</h2>
     <p className="lead">Local Authority: {name}</p>
-    {/* {lad.data && (
-              <LineChart
-                x={lad.data
-                  .filter((item) => item.parameter === "lambda")
-                  .map((item) => moment(item.date).format("YYYY-MM-DD"))}
-                y={lad.data
-                  .filter((item) => item.parameter === "lambda")
-                  .map((item) => item.mean)}
-                upper={lad.data
-                  .filter((item) => item.parameter === "lambda")
-                  .map((item) => item.upper)}
-                lower={lad.data
-                  .filter((item) => item.parameter === "lambda")
-                  .map((item) => item.lower)}
-              />
-            )} */}
+   
 
     <LineChart2
+  
      lad={lad}
       date={date}
       x={lad_data
-        .filter((item) => item.parameter === "yhat")
         .map((item) => moment(item.date).format("YYYY-MM-DD"))}
       y={lad_data
-        .filter((item) => item.parameter === "lambda")
         .map((item) => item.mean)}
       upper={lad_data
-        .filter((item) => item.parameter === "lambda")
         .map((item) => item.upper)}
       lower={lad_data
-        .filter((item) => item.parameter === "lambda")
-        .map((item) => item.lower)}
-      y1={lad_data
-        .filter((item) => item.parameter === "yhat")
-        .map((item) => item.mean)}
-      upper1={lad_data
-        .filter((item) => item.parameter === "yhat")
-        .map((item) => item.upper)}
-      lower1={lad_data
-        .filter((item) => item.parameter === "yhat")
         .map((item) => item.lower)}
     />
 
--- src/utils/loadData.js
+++ src/utils/loadData.js
@@ -1,32 +1,13 @@
 import { data } from "../assets/data_full.json"
 
+const dataForge = require('data-forge');
+
+
+
 function loadData(){
-    const dates = data.map((item) => item.date)
-    const unique_dates = [...new Set(dates)]
 
-    let indexed_by_date = {};
-
-    const filtered_data = data.filter((item) => item.parameter === "lambda")
-
-    const meanArray = filtered_data.map((item) => item.mean);
-    const dmin = Math.min(...meanArray);
-    const dmax = Math.max(...meanArray);
-
-
-    filtered_data.forEach((item) => {
-      if (item.parameter === "lambda"){
-      if(indexed_by_date[item.date]){
-        indexed_by_date[item.date][item.location] = item
-      }
-      else{
-        indexed_by_date[item.date]={}
-        indexed_by_date[item.date][item.location] = item
-        
-      }
-    }
-    })
-    
-    return([data,indexed_by_date,unique_dates,dmin,dmax]);
+   const dataframe = new dataForge.DataFrame(data).parseFloats("mean", "lower","upper")    
+   return(dataframe);
   }
 
 
