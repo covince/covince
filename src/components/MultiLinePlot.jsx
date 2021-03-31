43eeafbbf3492de7f63dde118827e4dd9479255c
Richard Goater <rg19@sanger.ac.uk>
2021-03-31 21:03:26 +0100

    Use colourblind-friendly colours on charts

--- src/components/MultiLinePlot.jsx
+++ src/components/MultiLinePlot.jsx
@@ -58,7 +58,14 @@ const CustomTooltip = ({ active, payload, label, percentage }) => {
   return null
 }
 
-const colors = ['red', 'green', 'blue', 'orange', 'pink', 'aqua', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000']
+// const colors = ['red', 'green', 'blue', 'orange', 'pink', 'aqua', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000']
+
+// https://jfly.uni-koeln.de/color/#pallet - colours picked off the image
+const colors = [
+  'rgb(44,183,236)', // sky blue
+  'rgb(243,100,13)', // vermillion
+  '#777'
+]
 
 const MultiLinePlot = ({ date, setDate, lad_data, parameter, type, width, height = 120, stroke = 'blueGray', className }) => {
   const chart = useMemo(() => {
