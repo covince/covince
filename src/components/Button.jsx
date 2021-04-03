c35a37647815396886c5d18ba23c75ec851e21d4
Richard Goater <rg19@sanger.ac.uk>
2021-04-03 23:44:25 +0100

    Start to add buton theming

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import classnames from 'classnames'
 
-const Button = (props) => (
-  <button
-    {...props}
-    className={classnames(props.className, [
-      'bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm',
-      'text-sm leading-5 font-medium text-gray-700',
-      'hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary'
-    ])}
-  />
+export const Button = ({ as = 'button', ...props }) => (
+  React.createElement(as, {
+    ...props,
+    className: classnames(props.className, [
+      'bg-white py-2 px-3 border rounded-md shadow-sm text-sm leading-5',
+      'focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary'
+    ])
+  })
 )
 
-export default Button
+export const DefaultButton = (props) =>
+  <Button
+    {...props}
+    className={classnames(props.className, [
+      'font-medium text-gray-700 hover:bg-gray-50 border-gray-300'
+    ])}
+  />
+
+export default DefaultButton
 
 export const PillButton = (props) => (
   <button
