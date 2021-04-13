8f9654159394451d7fa883d69343b9118b35ad6d
theosanderson <theo@theo.io>
2021-04-14 00:43:53 +0100

    Hacky version with useEffect in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React, { lazy, Suspense } from 'react'
+import React, { lazy, Suspense, useState, useEffect } from 'react'
 import NavBar from './components/NavBar'
 import Spinner from './components/Spinner'
 import AppContainer from './components/AppContainer'
+import axios from 'axios'
 
 const DataProvider = lazy(() => import('./components/DataProvider'))
 const Covid19 = lazy(() => import('./components/Covid19'))
@@ -13,15 +14,39 @@ const Loading = () => (
 )
 
 function App () {
+  const [data, setData] = useState(null)
+  const [tiles, setTiles] = useState(null)
+
+  useEffect(() => {
+    if (data === null) {
+      axios.get('./data/lists.json')
+        .then(res => {
+          setData(res.data)
+        })
+    }
+  }, [data])
+
+  useEffect(() => {
+    if (tiles === null) {
+      axios.get('./tiles/Local_Authority_Districts__December_2019__Boundaries_UK_BUC.json')
+        .then(res => {
+          res.data.features.reverse() // Hack to get Z order with England last
+          setTiles(res.data)
+        })
+    }
+  }, [tiles])
+
   return (
     <>
       <Suspense fallback={<Loading />}>
-        <NavBar />
+        {tiles && data
+          ? <> <NavBar />
         <AppContainer>
-            <DataProvider default_data_url="./data/lists.json" default_tiles_url="./tiles/Local_Authority_Districts__December_2019__Boundaries_UK_BUC.json">
-              <Covid19 />
-            </DataProvider>
-        </AppContainer>
+
+              <Covid19 data={data} tiles={tiles} />
+
+        </AppContainer> </>
+          : <Loading />}
       </Suspense>
     </>
   )
