ff625fa95ff3a6d3cc02c4c604f7b175f52c4cba
theosanderson <theo@theo.io>
2021-04-13 15:25:34 +0100

    Load tiles dynamically too

--- src/components/DataProvider.jsx
+++ src/components/DataProvider.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Spinner from './Spinner'
-import geojson from '../assets/Local_Authority_Districts__December_2019__Boundaries_UK_BUC.json'
-geojson.features.reverse()
 
 const DataProvider =
-  ({ children, default_data_url }) => {
+  ({ children, default_data_url, default_tiles_url }) => {
     const [data, setData] = useState(null)
+    const [tiles, setTiles] = useState(null)
 
     useEffect(() => {
       if (data === null) {
@@ -16,12 +15,23 @@ const DataProvider =
           })
       }
     }, [data])
-    if (data === null) {
+
+    useEffect(() => {
+      if (tiles === null) {
+        axios.get(default_tiles_url)
+          .then(res => {
+            res.data.features.reverse() // Hack to get Z order with England last
+            setTiles(res.data)
+          })
+      }
+    }, [tiles])
+
+    if (data === null | tiles === null) {
       return (<Spinner className='w-6 h-6 text-gray-500' />)
     }
 
     return (
-      React.cloneElement(children, { data: data, tiles: geojson })
+      React.cloneElement(children, { data: data, tiles: tiles })
     )
   }
 export default DataProvider
