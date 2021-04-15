510d81770e4ab73e811473840fa12ea985f589f1
theosanderson <theo@theo.io>
2021-04-15 01:18:11 +0100

    Asynchronous map/data. Suspense with react-query, and map parameters in JSON

---
+++ src/components/DataProvider.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react'
+import { useQuery } from 'react-query'
+import axios from 'axios'
+
+const DataProvider =
+  ({ children, default_data_url, default_tiles_url }) => {
+    const getData = async () => {
+      const { data } = await axios.get(default_data_url)
+      return data
+    }
+    const getTiles = async () => {
+      const { data } = await axios.get(default_tiles_url)
+      data.features.reverse()
+      return data
+    }
+
+    const { data } = useQuery('data', getData, { suspense: true })
+
+    const { data: tiles } = useQuery('tiles', getTiles, { suspense: true })
+
+    return (
+      React.cloneElement(children, { data: data, tiles: tiles })
+    )
+  }
+export default DataProvider
