db716435254c4324edab554879ff0d52b19d1f52
Richard Goater <rg19@sanger.ac.uk>
2021-04-15 14:05:38 +0100

    Few small tweaks:
    * remove tap colour on android
    * tweak header height to allow content to fit in Chrome
    * style scrollbar in filter section
    * remove unused css

--- src/components/FilterSection.jsx
+++ src/components/FilterSection.jsx
@@ -1,10 +1,12 @@
+import './FilterSection.css'
+
 import React from 'react'
 import classNames from 'classnames'
 
 import Spinner from './Spinner'
 
 const FilterSection = ({ className, children, loading }) => (
-  <div className={classNames('flex space-x-3 overflow-x-auto xl:overflow-visible pb-3 pl-1 xl:pl-0', className)}>
+  <div className={classNames('heron-filter-section flex space-x-3 overflow-x-auto xl:overflow-visible pl-1 xl:pl-0', className)}>
     { loading
       ? <div className='h-20 w-10 mb-6 flex justify-center items-start self-start'>
           <Spinner className='block w-5 h-5 text-white'/>
--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,7 @@ module.exports = {
       },
       spacing: {
         header: defaultTheme.spacing[16],
-        'header-md': defaultTheme.spacing[32],
+        'header-md': `calc(${defaultTheme.spacing[28]} + ${defaultTheme.spacing[2]})`,
         18: '4.5rem' // 3 x 1.5rem / spacing.6
       },
       gridTemplateRows: {
