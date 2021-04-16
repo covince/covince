0b92674a61e6799c3b46595cfe84576468b2f688
theosanderson <theo@theo.io>
2021-04-16 13:55:48 +0100

    upda

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,9 +5,13 @@ const NavBar = () => (
   <header className="h-header md:h-header-md bg-gradient-to-r from-blue-900 to-indigo-900">
     <div className="h-16 flex items-center container text-white px-4">
       <NavLink className="font-bold text-xl" to="/">
-        CovInce
+        CovInce GISAID data explorer
       </NavLink>
+     <div className="w-220 ml-10"> <p>Enabled by data from <img src="https://www.gisaid.org/fileadmin/gisaid/img/schild.png" className="w-20"></img></p>
+      </div>
+
     </div>
+
   </header>
 )
 
