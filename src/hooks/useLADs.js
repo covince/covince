190a52cedb9da7129576492078eff5f0705c6775
Richard Goater <rg19@sanger.ac.uk>
2021-04-16 13:20:26 +0100

    Back to Reducer (time-travel debugging would have helped here)

--- src/hooks/useLADs.js
+++ src/hooks/useLADs.js
@@ -1,41 +1,53 @@
-import { useEffect, useState, useMemo } from 'react'
+import { useEffect, useReducer } from 'react'
 import axios from 'axios'
 import useQueryAsState from './useQueryAsState'
 
 const useLAD = (dataPath) => {
   const [{ lad }, updateQuery] = useQueryAsState({ lad: 'national' })
-  const [{ currentLad, data }, storeResult] = useState({})
-
-  useEffect(() => {
-    axios.get(`${dataPath}/ltla/${lad}.json`)
-      .then(res => {
-        const new_data = res.data.data.map(x => {
-          if (x.parameter === 'p') {
+  const [state, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case 'LOADING':
+        return {
+          ...state,
+          loadingLad: action.payload,
+          status: 'LOADING'
+        }
+      case 'FETCHED': {
+        if (action.payload.lad !== state.loadingLad) return state
+        return {
+          ...state,
+          status: 'READY',
+          loadingLad: null,
+          currentLad: action.payload.lad,
+          data: action.payload.data.map(x => {
+            if (x.parameter === 'p') {
+              return {
+                ...x,
+                mean: x.mean * 100,
+                range: [x.lower * 100, x.upper * 100]
+              }
+            }
             return {
               ...x,
-              mean: x.mean * 100,
-              range: [x.lower * 100, x.upper * 100]
+              range: [x.lower, x.upper]
             }
-          }
-          return {
-            ...x,
-            range: [x.lower, x.upper]
-          }
-        })
-        storeResult({ currentLad: lad, data: new_data })
+          })
+        }
+      }
+    }
+  }, { status: 'INIT' })
+
+  useEffect(() => {
+    dispatch({ type: 'LOADING', payload: lad })
+    axios.get(`${dataPath}/ltla/${lad}.json`)
+      .then(res => {
+        dispatch({ type: 'FETCHED', payload: { lad, data: res.data.data } })
       })
   }, [lad])
 
   const actions = {
     load: (lad) => updateQuery({ lad })
   }
-
-  const state = useMemo(() => ({
-    status: lad === currentLad ? 'READY' : 'LOADING',
-    loadingLad: lad,
-    currentLad,
-    data
-  }))
 
   return [state, actions]
 }
