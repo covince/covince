1ca24b87cad8380e4951904e86fea92bb1d4d511
theosanderson <theo@theo.io>
2021-04-21 16:47:03 +0100

    Allow chart definitions

--- src/components/LocalIncidence.jsx
+++ src/components/LocalIncidence.jsx
@@ -3,22 +3,10 @@ import Measure from 'react-measure'
 import classNames from 'classnames'
 
 import Chart from './Chart'
-import Checkbox from './Checkbox'
 
-import useQueryAsState from '../hooks/useQueryAsState'
-
-function LocalIncidence ({ values, date, setDate, className, isMobile = false, lineColor, colors }) {
+function LocalIncidence ({ chartDefinitions, values, date, setDate, className, isMobile = false, lineColor, colors }) {
   const area_data = useMemo(() => values || [], [values])
-
-  const [{ proportion = 'area', incidence = 'line' }, updateQuery] = useQueryAsState()
-
-  const handlePropChange = useCallback(function (event) {
-    updateQuery({ proportion: event.target.checked ? undefined : 'line' })
-  }, [updateQuery])
-
-  const handleLambdaChange = useCallback(function (event) {
-    updateQuery({ incidence: event.target.checked ? 'area' : undefined })
-  }, [updateQuery])
+  const numCharts = chartDefinitions.length
 
   const [width, setWidth] = useState(0)
 
@@ -32,60 +20,25 @@ function LocalIncidence ({ values, date, setDate, className, isMobile = false, l
       }}
     >
       {({ measureRef }) => (
-        <div ref={measureRef} className={classNames('grid grid-rows-3 gap-2', className)}>
-          <Chart
-            colors = {colors}
-            width={width}
-            isMobile={isMobile}
-            heading='Incidence'
-            controls={
-              <Checkbox
-                id='lambda_display_type'
-                checked={incidence === 'area'}
-                label='Stack'
-                onChange={handleLambdaChange}
-                toggle
-              />
-            }
-            area_data={area_data}
-            date={date}
-            setDate={setDate}
-            parameter='lambda'
-            type={incidence}
-            stroke={lineColor}
-          />
-          <Chart
-            colors= {colors}
-            width={width}
-            isMobile={isMobile}
-            heading='Proportion'
-            controls={
-              <Checkbox
-                id='proportion_display_type'
-                checked={proportion === 'area'}
-                label='Stack'
-                onChange={handlePropChange}
-                toggle
-              />
-            }
-            area_data={area_data}
-            date={date}
-            setDate={setDate}
-            parameter='p'
-            type={proportion}
-            stroke={lineColor}
-          />
-          <Chart
-          colors = {colors}
-            width={width}
-            isMobile={isMobile}
-            heading='R'
-            area_data={area_data}
-            date={date}
-            setDate={setDate}
-            parameter='R'
-            stroke={lineColor}
-          />
+        <div ref={measureRef} className={classNames(`grid grid-rows-${numCharts} gap-2`, className)}>
+          {
+            chartDefinitions.map(chart => <Chart
+              colors = {colors}
+              width={width}
+              isMobile={isMobile}
+              heading={chart.heading}
+              area_data={area_data}
+              date={date}
+              setDate={setDate}
+              parameter={chart.parameter}
+              key={chart.parameter}
+              defaultType={chart.defaultType}
+              stroke={lineColor}
+              allowStack={true}
+              numCharts ={numCharts}
+            />)
+          }
+
         </div>
       )}
     </Measure>
