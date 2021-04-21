85c12a2245ea114b42606cd2ef6db39756f1c25c
theosanderson <theo@theo.io>
2021-04-22 00:21:03 +0100

    fix popup bug

--- src/components/Chloropleth.jsx
+++ src/components/Chloropleth.jsx
@@ -314,7 +314,7 @@ const Chloropleth = (props) => {
             onHover={e => {
               const [feature] = e.features
               if (feature && 'value' in feature.properties) {
-                if (feature.properties.latitude === undefined) {
+                if (feature.properties.lat === undefined) {
                   // Hack where if no central point is specified
                   // we use mouse position for popup
                   feature.properties.lat = e.lngLat[1]
