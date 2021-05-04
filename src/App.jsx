a6783f96c133a953c2c29b13874324df661f9ece
Richard Goater <rg19@sanger.ac.uk>
2021-05-04 23:25:45 +0100

    Callback for data version (#68)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useState, useMemo } from 'react'
 
 import NavBar from './components/NavBar'
 import Spinner from './components/Spinner'
 import AppContainer from './components/AppContainer'
 import Covince from './Covince'
+import { format } from 'date-fns'
 
 const Loading = () => (
   <div className='fixed inset-0 grid place-content-center'>
@@ -12,12 +13,23 @@ const Loading = () => (
 )
 
 function App () {
+  const [info, setInfo] = useState(null)
+  const formattedVersion = useMemo(() => {
+    if (info) return format(new Date(info.lastModified), 'dd MMMM yyy, HH:mm')
+    return null
+  }, [info])
+
   return (
     <>
       <Suspense fallback={<Loading />}>
-        <NavBar />
+        <NavBar>
+          { formattedVersion &&
+            <p className='text-sm text-right text-indigo-100'>
+              Data updated <strong className='block'>{formattedVersion}</strong>
+            </p> }
+        </NavBar>
         <AppContainer>
-          <Covince />
+          <Covince onLoad={setInfo} />
         </AppContainer>
       </Suspense>
     </>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-const NavBar = () => (
+const NavBar = ({ children }) => (
   <header className="h-header md:h-header-md bg-gradient-to-r from-blue-900 to-indigo-900">
-    <div className="h-16 flex items-center container text-white px-4">
+    <div className="h-16 flex items-center justify-between container text-white px-4">
       <NavLink className="font-bold text-xl" to="/">
         CovInce
       </NavLink>
+      {children}
     </div>
   </header>
 )
