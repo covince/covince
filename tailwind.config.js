60f0d966b0e268b391847099aeb9e7238d16e1e7
Theo Sanderson <theo@sndrsn.co.uk>
2021-06-21 15:21:09 +0100

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,12 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-const colors = require('tailwindcss/colors')
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
+const covinceConfig = require("covince/tailwind.config");
 
 module.exports = {
+  ...covinceConfig,
   purge: {
-    content: [
-      './src/**/*.jsx'
-    ]
+    content: ["./src/**/*.jsx", "./node_modules/covince/src/**/*.jsx"],
   },
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    fontFamily: {
-      sans: defaultTheme.fontFamily.sans,
-      heading: defaultTheme.fontFamily.sans
-    },
-    container: false,
-    extend: {
-      colors: {
-        gray: colors.blueGray,
-        primary: colors.blue[700],
-        heading: 'theme("colors.gray.600")',
-        subheading: 'theme("colors.gray.500")'
-      },
-      spacing: {
-        header: defaultTheme.spacing[16],
-        'header-md': `calc(${defaultTheme.spacing[28]} + ${defaultTheme.spacing[2]})`,
-        18: '4.5rem' // 3 x 1.5rem / spacing.6
-      },
-      gridTemplateRows: {
-        '1-full': '100%'
-      }
-    }
-  },
-  variants: {
-    extend: {}
-  },
-  plugins: [
-    require('@tailwindcss/forms')
-  ]
-}
+  plugins: [],
+};
