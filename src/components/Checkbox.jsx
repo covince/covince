42c70bbc6d06001da7bcd6b4c632bbaa869c991c
Richard Goater <rg19@sanger.ac.uk>
2021-08-23 12:32:18 +0100

    Scope checkbox style again

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -13,7 +13,7 @@ const Checkbox = ({ id, name, children, label = children, checked, onChange, cla
         checked={checked}
         onChange={onChange}
         type="checkbox"
-        className="bg-none outline-none focus:ring-2 focus:ring-current dark:focus:ring-offset-gray-700 ring-offset-2 h-4 w-4 text-current border-gray-300 rounded"
+        className="covince-checkbox outline-none focus:ring-2 focus:ring-current dark:focus:ring-offset-gray-700 ring-offset-2 h-4 w-4 text-current border-gray-300 rounded"
       />
       { checked && <CheckMark className='absolute h-2.5 w-2.5 stroke-current text-white pointer-events-none dark:text-gray-800' /> }
     </div>
