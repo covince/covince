cdd2a4a194e6ef89fce5c45bb562ddac4e51ae9a
Richard Goater <rg19@sanger.ac.uk>
2021-09-24 18:48:00 +0100

    Add disabled state to checkbox

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,10 +1,8 @@
-import './Checkbox.css'
-
 import React from 'react'
 import classNames from 'classnames'
 import { FaCheck as CheckMark } from 'react-icons/fa'
 
-const Checkbox = ({ id, name, children, label = children, checked, onChange, className, style }) => (
+const Checkbox = ({ id, name, children, label = children, checked, onChange, className, style, disabled }) => (
   <div className={classNames('flex items-center', className)} style={style}>
     <div className="flex items-center justify-center h-5 relative">
       <input
@@ -12,8 +10,9 @@ const Checkbox = ({ id, name, children, label = children, checked, onChange, cla
         name={name}
         checked={checked}
         onChange={onChange}
+        disabled={disabled}
         type="checkbox"
-        className="covince-checkbox outline-none focus:ring-2 focus:ring-current dark:focus:ring-offset-gray-700 ring-offset-2 h-4 w-4 text-current border-gray-300 rounded"
+        className="covince-checkbox checked:bg-none outline-none focus:ring-2 focus:ring-current dark:focus:ring-offset-gray-700 ring-offset-2 h-4 w-4 text-current border-gray-300 rounded disabled:bg-gray-200"
       />
       { checked && <CheckMark className='absolute h-2.5 w-2.5 stroke-current text-white pointer-events-none dark:text-gray-800' /> }
     </div>
