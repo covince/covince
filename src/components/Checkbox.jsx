a03b71ba6cf26c7848d1dc844e2ca6425e258430
Richard Goater <rg19@sanger.ac.uk>
2021-09-29 16:43:01 +0100

    Add title to checkbox

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 import classNames from 'classnames'
 import { FaCheck as CheckMark } from 'react-icons/fa'
 
-const Checkbox = ({ id, name, children, label = children, checked, onChange, className, style, disabled }) => (
+const Checkbox = ({ id, name, children, label = children, checked, onChange, className, style, disabled, title }) => (
   <div className={classNames('flex items-center', className)} style={style}>
     <div className="flex items-center justify-center h-5 relative">
       <input
+        checked={checked}
+        className="covince-checkbox checked:bg-none outline-none focus:ring-2 focus:ring-current dark:focus:ring-offset-gray-700 ring-offset-2 h-4 w-4 text-current border-gray-300 rounded disabled:bg-gray-200"
+        disabled={disabled}
         id={id}
         name={name}
-        checked={checked}
         onChange={onChange}
-        disabled={disabled}
+        title={title}
         type="checkbox"
-        className="covince-checkbox checked:bg-none outline-none focus:ring-2 focus:ring-current dark:focus:ring-offset-gray-700 ring-offset-2 h-4 w-4 text-current border-gray-300 rounded disabled:bg-gray-200"
       />
       { checked && <CheckMark className='absolute h-2.5 w-2.5 stroke-current text-white pointer-events-none dark:text-gray-800' /> }
     </div>
